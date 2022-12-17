618ce5a9a084a97b955808be7650d9e398e379a0
nns7 <nns7snn@gmail.com>
2022-12-18 00:47:07 +0900

    :tada: 書籍レビュー一覧画面を作成。

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,135 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  Divider,
+  Grid,
+  Typography,
+} from "@mui/material";
+import axios from "axios";
 import * as React from "react";
-import { Link } from "react-router-dom";
+import { useCookies } from "react-cookie";
+import { useSelector } from "react-redux";
+import { Book } from "../common/book.type";
+import { RootState } from "../common/rootState.type";
+import Footer from "../components/Footer";
 
 const Home = () => {
+  const auth = useSelector((state: RootState) => state.auth.isSignIn);
+  const [books, setBooks] = React.useState<[] | Book[]>([]);
+  const [, setErrorMessage] = React.useState("");
+  const [cookies] = useCookies();
+
+  React.useEffect(() => {
+    const url = auth
+      ? `${process.env.REACT_APP_API_URL}/books`
+      : `${process.env.REACT_APP_API_URL}/public/books`;
+    const options = {
+      headers: {
+        authorization: `Bearer ${cookies.token}`,
+      },
+      params: {
+        offset: 0,
+      },
+    };
+
+    axios
+      .get(url, options)
+      .then((res) => {
+        setBooks(res.data);
+      })
+      .catch((err) => {
+        setErrorMessage(`書籍一覧の取得に失敗しました。 ${err}`);
+      });
+  }, []);
+
   return (
-    <>
-      <h1>書籍レビュー一覧画面</h1>
-      <div>
-        <Link to={`/signup`}>ユーザー作成画面へ</Link>
-        <br />
-        <Link to={`/login`}>ログイン画面へ</Link>
-        <br />
-        <Link to={`/profile`}>ユーザー情報編集画面へ</Link>
-        <br />
-        <Link to={`/new`}>書籍レビュー登録画面へ</Link>
-        <br />
-        <Link to={`/detail/0`}>書籍レビュー詳細画面へ</Link>
-        <br />
-        <Link to={`/edit/0`}>書籍レビュー編集画面へ</Link>
-      </div>
-    </>
+    <Grid container component="main" maxWidth="xs">
+      {auth ? (
+        <></>
+      ) : (
+        <>
+          <Grid item xs={1} sm={1} md={2} />
+          <Grid item xs={10} sm={10} md={8}>
+            <Box
+              sx={{
+                mt: 8,
+                mx: 4,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+              }}
+            >
+              <Container maxWidth="sm">
+                <Typography
+                  component="h1"
+                  variant="h2"
+                  align="center"
+                  color="text.primary"
+                  gutterBottom
+                >
+                  bookmailとは
+                </Typography>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  color="text.secondary"
+                  paragraph
+                >
+                  ようこそbookmailへ！
+                  <br />
+                  bookmailでは書籍のレビューを投稿することができます。
+                </Typography>
+              </Container>
+            </Box>
+          </Grid>
+          <Grid item xs={1} sm={1} md={2} />
+        </>
+      )}
+
+      <Grid
+        container
+        spacing={4}
+        sx={{
+          mt: 1,
+        }}
+      >
+        {books.map((book: Book) => (
+          <Grid item key={book.id} xs={12} sm={6} md={3}>
+            <Card
+              sx={{ height: "100%", display: "flex", flexDirection: "column" }}
+            >
+              <CardContent sx={{ flexGrow: 1 }}>
+                <Typography variant="h5" component="h2">
+                  {book.title}
+                </Typography>
+                <Typography variant="subtitle1">{book.detail}</Typography>
+                <Typography variant="caption">URL:{book.url}</Typography>
+                <Divider />
+                <Typography variant="body1" sx={{ pt: 2 }}>
+                  {book.review}
+                </Typography>
+                <Typography align="right" variant="body2">
+                  投稿者:{book.reviewer}
+                </Typography>
+                {book.isMine === true ? (
+                  <CardActions>
+                    <Button size="small">編集</Button>
+                  </CardActions>
+                ) : (
+                  <></>
+                )}
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+
+      <Footer />
+    </Grid>
   );
 };
 
