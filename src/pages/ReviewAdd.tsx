dff4706332464776f6b5fbc985185a8d073b3106
nns7 <nns7snn@gmail.com>
2022-12-18 01:57:57 +0900

    :tada: APIからレビューを投稿できるよう実装。

--- src/pages/ReviewAdd.tsx
+++ src/pages/ReviewAdd.tsx
@@ -1,16 +1,54 @@
 import { MenuBook } from "@mui/icons-material";
 import {
+  Alert,
   Avatar,
   Box,
   Button,
+  Collapse,
   Grid,
+  IconButton,
   TextField,
   Typography,
 } from "@mui/material";
+import axios from "axios";
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer";
+import CloseIcon from "@mui/icons-material/Close";
+import { useCookies } from "react-cookie";
 
 const ReviewAdd = () => {
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = React.useState<string>();
+  const [errorOpen, setErrorOpen] = React.useState<boolean>(false);
+  const [cookies] = useCookies();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const data = {
+      title: formData.get("title"),
+      url: formData.get("url"),
+      detail: formData.get("detail"),
+      review: formData.get("review"),
+    };
+    const options = {
+      headers: {
+        authorization: `Bearer ${cookies.token}`,
+      },
+    };
+
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/books`, data, options)
+      .then(() => {
+        navigate("/");
+      })
+      .catch((err) => {
+        setErrorMessage(`ユーザー作成に失敗しました。 ${err}`);
+        setErrorOpen(true);
+      });
+  };
+
   return (
     <Grid container component="main" maxWidth="xs">
       <Grid item xs={1} sm={1} md={2} />
@@ -29,7 +67,12 @@ const ReviewAdd = () => {
           <Typography component="h1" variant="h5">
             書籍レビュー投稿
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleSubmit}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
@@ -85,6 +128,32 @@ const ReviewAdd = () => {
               投稿
             </Button>
           </Box>
+        </Box>
+      </Grid>
+
+      <Grid item xs={1} sm={1} md={2} />
+      <Grid item xs={10} sm={10} md={8}>
+        <Box sx={{ width: "100%" }}>
+          <Collapse in={errorOpen}>
+            <Alert
+              severity="error"
+              action={
+                <IconButton
+                  aria-label="close"
+                  color="inherit"
+                  size="small"
+                  onClick={() => {
+                    setErrorOpen(false);
+                  }}
+                >
+                  <CloseIcon fontSize="inherit" />
+                </IconButton>
+              }
+              sx={{ mb: 2 }}
+            >
+              {errorMessage}
+            </Alert>
+          </Collapse>
         </Box>
       </Grid>
 
